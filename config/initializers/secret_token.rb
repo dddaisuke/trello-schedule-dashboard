36dfdf50276e6b1abbb2403778c65e22e83e0fa3
Daisuke Yamashita <dddaisuke@gmail.com>
2013-08-05 16:13:36 +0900

    rails new trello-schedule-dashboard -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrelloScheduleDashboard::Application.config.secret_key_base = 'ff63cad8f97981d79ec957f2ee74a023e663dbdb41e403c1f5402c2c5a839ff5eaa412e64c96f78cd7a87c2adacd021a493b2e6e6e0b56109cef5c6ac718028e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrelloScheduleDashboard::Application.config.session_store :cookie_store, key: '_trello-schedule-dashboard_session'
