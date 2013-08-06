a6653bd1155ae196fd6893a8cb54569c9524544f
Daisuke Yamashita <dddaisuke@gmail.com>
2013-08-06 09:30:08 +0900

    Add basic authentication

--- app/controllers/cache_controller.rb
+++ app/controllers/cache_controller.rb
@@ -1,4 +1,6 @@
 class CacheController < ApplicationController
+  http_basic_authenticate_with name: ENV['TDD_NAME'], password: ENV['TDD_PASS']
+
   def clear
     Rails.cache.clear
     redirect_to dashboard_index_path, flash: { alert_message: 'Clear cache!' }
