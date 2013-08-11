016338da8bb39d8192d108f6606a0ff0a739ca11
Daisuke Yamashita <dddaisuke@gmail.com>
2013-08-11 10:10:08 +0900

    Fix unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,7 @@
 worker_processes 2
+@app = '/home/deployer/trello-schedule-dashboard/current'
 listen '/tmp/unicorn.sock'
+pid '/tmp/unicorn.pid'
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 preload_app true
